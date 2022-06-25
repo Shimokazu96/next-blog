2290bba454cce5a4f6fee24c53e1b4fd0fb687ad
Shimokazu96 <aloe1dental@gmail.com>
2022-06-25 14:17:31 +0900

    sidebarコンポーネント化

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react'
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import Sidebar from "@/components/sidebar";
 
 type LayoutProps = Required<{
     readonly children: ReactElement;
@@ -9,7 +10,17 @@ type LayoutProps = Required<{
 export const Layout = ({ children }: LayoutProps) => (
     <>
         <Header />
-        {children}
+        <div className="bg-white py-6 sm:py-8 lg:py-12">
+            <div className="max-w-screen-lg px-4 md:px-8 mx-auto">
+                <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-3 gap-8">
+                    <Sidebar />
+                    {children}
+                </div>
+            </div>
+        </div>
         <Footer />
+        {/* <Header />
+        {children}
+        <Footer /> */}
     </>
 );
