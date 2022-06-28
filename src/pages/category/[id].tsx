b05c5e3efd016b92bc2aed60210dd104a9727157
1st-shimooka <78331743+ShimookaKazuhiko@users.noreply.github.com>
2022-06-28 17:19:17 +0900

    記事一覧をコンポーネント化

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -1,8 +1,8 @@
-import { parseISO, format } from "date-fns";
-import ja from "date-fns/locale/ja";
 import { wpClient } from "@/lib/wpapi";
-import Link from "next/link";
 import { Layout } from "@/components/layout";
+import ArticleList from "@/components/article-list";
+import ArticleTitle from "@/components/article-title";
+import Pagination from "@/components/pagination";
 
 export const getStaticPaths = async () => {
     const categories = await wpClient.categories();
@@ -19,10 +19,18 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async ({ params }) => {
     const posts = await wpClient.posts().param("categories", params.id);
     const category_name = await wpClient.categories().id(params.id);
+    const pages = await wpClient
+        .posts()
+        .param("categories", params.id)
+        .get()
+        .then((response) => {
+            return JSON.parse(JSON.stringify(response["_paging"]));
+        });
     return {
         props: {
             posts: posts,
             category_name: category_name.name,
+            pages: pages,
         },
     };
 };
@@ -31,55 +39,9 @@ const Category = (props) => {
     return (
         <Layout>
             <div className="lg:col-span-2">
-                <div className="border-b pb-4 md:pb-6">
-                    <h2 className="text-gray-800 text-lg lg:text-xl font-bold">
-                        {props.category_name}の記事一覧
-                    </h2>
-                </div>
-
-                <div className="divide-y">
-                    {props.posts.map(
-                        (post) => (
-                            (
-                                <div
-                                    key={post.id}
-                                    className="flex flex-col gap-3 py-4 md:py-8 hover:text-blue-800 active:text-blue-700"
-                                >
-                                    <Link href={`${process.env.API_URL}/posts/${post.slug}`}>
-                                        <a>
-                                            <div>
-                                                <div className="block text-sm font-bold">
-                                                    {post.title.rendered}
-                                                </div>
-                                                <span className="block font-semibold text-gray-500 text-sm ">
-                                                    {format(new Date(post.date), "yyyy-MM-dd")}
-                                                </span>
-                                            </div>
-
-                                            <div className="flex gap-0.5 -ml-1"></div>
-
-                                            <div
-                                                dangerouslySetInnerHTML={{
-                                                    __html: post.excerpt.rendered,
-                                                }}
-                                                className="text-gray-600 font-semibold"
-                                            />
-                                        </a>
-                                    </Link>
-                                </div>
-                            )
-                        )
-                    )}
-                </div>
-
-                <div className="border-t pt-6">
-                    <a
-                        href="#"
-                        className="flex items-center text-blue-700 text-lg font-semibold transition duration-100 gap-0.5"
-                    >
-                        Read all reviews
-                    </a>
-                </div>
+                <ArticleTitle category_name={props.category_name} />
+                <ArticleList posts={props.posts} />
+                <Pagination pages={props.pages} />
             </div>
         </Layout>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,41 @@
-import { parseISO, format } from "date-fns";
-import ja from "date-fns/locale/ja";
 import { useState, useEffect } from "react";
 import { wpClient } from "@/lib/wpapi";
 import type { NextPage } from "next";
-import Link from "next/link";
 import { Layout } from "@/components/layout";
+import ArticleList from "@/components/article-list";
+import ArticleTitle from "@/components/article-title";
 import Pagination from "@/components/pagination";
 
 export const getStaticProps = async () => {
-    // const data = await wpClient.posts().param('page',10); // perPage()で表示ページ数指定
-    const data = await wpClient.posts().perPage(10).param("page", 1); // perPage()で表示ページ数指定
+    const data = await wpClient.posts().perPage(10); // perPage()で表示ページ数指定
+    const pages = await wpClient
+        .posts()
+        .get()
+        .then((response) => {
+            return JSON.parse(JSON.stringify(response["_paging"]));
+        });
 
     return {
         props: {
             posts: data,
+            category_name: "",
+            pages: pages,
         },
     };
 };
 
 const Home: NextPage = (props) => {
-    const [page, setPage] = useState([]);
-    useEffect(() => {
-        wpClient
-            .posts()
-            .get()
-            .then((response) => {
-                setPage(response["_paging"]);
-            });
-    }, []);
     return (
-        <Layout>
-            <div className="lg:col-span-2">
-                <div className="border-b pb-4 md:pb-6">
-                    <h2 className="text-gray-800 text-lg lg:text-xl font-bold">ブログ記事一覧</h2>
+        console.log(props),
+        (
+            <Layout>
+                <div className="lg:col-span-2">
+                    <ArticleTitle posts={props.category_name} />
+                    <ArticleList posts={props.posts} />
+                    <Pagination pages={props.pages} />
                 </div>
-
-                <div className="divide-y">
-                    {props.posts.map((post) => (
-                        <div
-                            key={post.id}
-                            className="flex flex-col gap-3 py-4 md:py-8 hover:text-blue-800 active:text-blue-700"
-                        >
-                            <Link href={`${process.env.API_URL}/posts/${post.slug}`}>
-                                <a>
-                                    <div>
-                                        <div className="block text-sm font-bold">
-                                            {post.title.rendered}
-                                        </div>
-                                        <span className="block font-semibold text-gray-500 text-sm ">
-                                            {format(new Date(post.date), "yyyy-MM-dd")}
-                                        </span>
-                                    </div>
-
-                                    <div className="flex gap-0.5 -ml-1"></div>
-
-                                    <div
-                                        dangerouslySetInnerHTML={{
-                                            __html: post.excerpt.rendered,
-                                        }}
-                                        className="text-gray-600 font-semibold"
-                                    />
-                                </a>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-                <Pagination />
-            </div>
-        </Layout>
+            </Layout>
+        )
     );
 };
 
